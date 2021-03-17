84aaaa1387b748497f6817a991108cdc567f3826
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-17 08:37:25 +0530

    [AiswaryaChoudhury] Add : Player Rolls The Die Feature Implemented

---
+++ SnakeAndLadder/GamePlay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class GamePlay
+    {
+        static int STARTING_POSITION = 0;
+        public int rollDice()
+        {
+            Random random = new Random();
+            STARTING_POSITION = random.Next(1, 7);
+            return (STARTING_POSITION == 0 ? 1 : STARTING_POSITION);
+        }
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,12 +4,11 @@ namespace SnakeAndLadder
 {
     class Program
     { 
-        static int STARTING_POSITION = 0;
         static void Main(string[] args)
         { 
             Console.WriteLine("Welcome to Snake and Ladder game Player");
-            Console.WriteLine("Player is at Position :" + STARTING_POSITION);
-
+            GamePlay game = new GamePlay();
+            Console.WriteLine("Die Rolled:" + game.rollDice());
         }
     }
 }
