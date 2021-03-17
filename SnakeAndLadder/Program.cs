615fc7e38a55efdad32c4b0c82f9188bcf91a80d
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-17 08:25:12 +0530

    [AiswaryaChoudhury] Add : Project Structure Implemented

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,13 +4,9 @@ namespace SnakeAndLadder
 {
     class Program
     {
-        static int STARTING_POSITION = 0;
         static void Main(string[] args)
         { 
             Console.WriteLine("Welcome to Snake and Ladder game Player");
-            Console.WriteLine("Player is at Position :" + STARTING_POSITION);
-
-
-        }
+         }
     }
 }
