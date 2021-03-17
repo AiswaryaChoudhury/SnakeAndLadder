a04161bfce0fce3041aed686f6c706ba14437bbe
AiswaryaChoudhury <choudary.aiswarya8@gmail.com>
2021-03-17 08:29:40 +0530

    [AiswaryaChoudhury] Add : Game Played with Single Player at Start Position Feature Implemented

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -3,10 +3,13 @@
 namespace SnakeAndLadder
 {
     class Program
-    {
+    { 
+        static int STARTING_POSITION = 0;
         static void Main(string[] args)
         { 
             Console.WriteLine("Welcome to Snake and Ladder game Player");
-         }
+            Console.WriteLine("Player is at Position :" + STARTING_POSITION);
+
+        }
     }
 }
